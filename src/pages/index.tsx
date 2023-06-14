a6644ea3d17aee7ff091d4685e1bd5b4c7994c8b
joyxof <129546165+joyxof@users.noreply.github.com>
2023-06-15 02:16:19 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ function addHighlightText(element: any) {
 
 const Home: NextPage = () => {
   const [file, setFile] = useState<File | string>('/github-privacy.pdf');
-  const disabledUpload = false;
+  const disabledUpload = true;
   const [numPages, setNumPages] = useState(null);
   const [loading, setLoading] = useState(false);
   const pdfRef = useRef<unknown>();
