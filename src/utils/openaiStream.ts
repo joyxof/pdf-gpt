bc1f769bd09bedda8268da305ce8ab5f7547371c
joyxof <129546165+joyxof@users.noreply.github.com>
2023-06-15 23:44:43 +0800

    Update openaiStream.ts

--- src/utils/openaiStream.ts
+++ src/utils/openaiStream.ts
@@ -5,6 +5,17 @@ export const OpenAIStream = async (prompt: string, apiKey: string) => {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
+
+  const prompt = """
+    You are aknowledgeable assistant that accurately \
+    answers queries. Use the text below, delimited by triple quotes \
+    extract to form your answer, but avoid copying word-for-word \
+    from the context.outputted in markdown format. If you are unsure \
+    and the answer is not explicitly written in the documentation, \
+    say "Sorry, I don't know how to help with that.
+      '''${prompt}'''
+  """
+      
   const res = await fetch('https://joyxof.openai.azure.com/openai/deployments/redfox1/completions?api-version=2023-05-15', {
     headers: {
       'api-key': apiKey,
