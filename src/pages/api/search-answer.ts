4a047abb3f22ff2860fbed620676781dfed915f3
joyxof <129546165+joyxof@users.noreply.github.com>
2023-06-16 01:30:53 +0800

    Update search-answer.ts

--- src/pages/api/search-answer.ts
+++ src/pages/api/search-answer.ts
@@ -20,13 +20,4 @@ const handler = async (req: Request): Promise<Response> => {
   }
 };
 
-const prompt: string = `
-    You are aknowledgeable assistant that accurately \
-    answers queries. Use the text below, delimited by triple quotes \
-    extract to form your answer, but avoid copying word-for-word \
-    from the context.outputted in markdown format. If you are unsure \
-    and the answer is not explicitly written in the documentation, \
-    say "Sorry, I don't know how to help with that."
-  '''${prompt}'''
-  `;
 export default handler;
