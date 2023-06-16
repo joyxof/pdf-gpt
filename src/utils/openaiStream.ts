8ad06655c195cbd7bc44e535a3adeca19a10d7c6
joyxof <129546165+joyxof@users.noreply.github.com>
2023-06-16 19:16:04 +0800

    Update openaiStream.ts

--- src/utils/openaiStream.ts
+++ src/utils/openaiStream.ts
@@ -5,8 +5,16 @@ export const OpenAIStream = async (prompt: string, apiKey: string) => {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
-
-  const res = await fetch('https://joyxof.openai.azure.com/openai/deployments/redfox1/chat/completions?api-version=2023-06-01-preview', {
+  const prompt = `
+    You are aknowledgeable assistant that accurately answers queries. \
+    Given the following context below, delimited by triple quotes extract. \
+    but avoid copying word-for-word from the context.Please answer in \
+    concise Chinese and keep it within 400 words.If you are unsure and \
+    the answer is not explicitly written in the context, say "Sorry, I don't know." \
+    '''${Prompt}'''
+    `;
+  
+  const res = await fetch('https://joyxof.openai.azure.com/openai/deployments/redfox1/completions?api-version=2023-06-01-preview', {
     headers: {
       'api-key': apiKey,
       'Content-Type': 'application/json',
@@ -14,20 +22,20 @@ export const OpenAIStream = async (prompt: string, apiKey: string) => {
     method: 'POST',
     body: JSON.stringify({
       // model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'system',
-          content: `You are aknowledgeable assistant that accurately answers queries. \
-            Given the following sections to form your answer, but avoid copying \
-            word-for-word from the context.Please answer in concise Chinese and \
-            keep it within 400 words.If you are unsure and the answer is not \
-            explicitly written in the context, say "Sorry, I don't know." \
-        `},
-        {
-          role: 'user',
-          content: prompt,
-        }
-      ],
+      // messages: [
+        // {
+          // role: 'system',
+          // content: `You are aknowledgeable assistant that accurately answers queries. \
+            // Given the following sections to form your answer, but avoid copying \
+            // word-for-word from the context.Please answer in concise Chinese and \
+            // keep it within 400 words.If you are unsure and the answer is not \
+            // explicitly written in the context, say "Sorry, I don't know." \
+        // `},
+        // {
+          // role: 'user',
+          // content: prompt,
+        // }
+      // ],
       temperature: 0.1,
       max_tokens: 512,
       stream: true
