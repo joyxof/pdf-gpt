369eae5df30bd51752fa42beeb5df768b2df23d2
joyxof <129546165+joyxof@users.noreply.github.com>
2023-06-16 19:48:23 +0800

    Update openaiStream.ts

--- src/utils/openaiStream.ts
+++ src/utils/openaiStream.ts
@@ -5,16 +5,16 @@ export const OpenAIStream = async (Prompt: string, apiKey: string) => {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
-  const prompt = `
-    You are aknowledgeable assistant that accurately answers queries. \
-    Given the following context below, delimited by triple quotes extract. \
-    but avoid copying word-for-word from the context.Please answer in \
-    concise Chinese and keep it within 400 words.If you are unsure and \
-    the answer is not explicitly written in the context, say "Sorry, I don't know." \
-    '''${Prompt}'''
-    `;
+  // const prompt = `
+    // You are aknowledgeable assistant that accurately answers queries. \
+    // Given the following context below, delimited by triple quotes extract. \
+    // but avoid copying word-for-word from the context.Please answer in \
+    // concise Chinese and keep it within 400 words.If you are unsure and \
+    // the answer is not explicitly written in the context, say "Sorry, I don't know." \
+    // '''${Prompt}'''
+    // `;
   
-  const res = await fetch('https://joyxof.openai.azure.com/openai/deployments/redfox1/completions?api-version=2023-05-15', {
+  const res = await fetch('https://joyxof.openai.azure.com/openai/deployments/redfox1/chat/completions?api-version=2023-05-15', {
     headers: {
       'api-key': apiKey,
       'Content-Type': 'application/json',
@@ -22,21 +22,20 @@ export const OpenAIStream = async (Prompt: string, apiKey: string) => {
     method: 'POST',
     body: JSON.stringify({
       // model: 'gpt-3.5-turbo',
-      // messages: [
-        // {
-          // role: 'system',
-          // content: `You are aknowledgeable assistant that accurately answers queries. \
-            // Given the following sections to form your answer, but avoid copying \
-            // word-for-word from the context.Please answer in concise Chinese and \
-            // keep it within 400 words.If you are unsure and the answer is not \
-            // explicitly written in the context, say "Sorry, I don't know." \
-        // `},
-        // {
-          // role: 'user',
-          // content: prompt,
-        // }
-      // ],
-      prompt,
+      messages: [
+        {
+          role: 'system',
+          content: `You are aknowledgeable assistant that accurately answers queries. \
+            Given the following sections to form your answer, but avoid copying \
+            word-for-word from the context.Please answer in concise Chinese and \
+            keep it within 400 words.If you are unsure and the answer is not \
+            explicitly written in the context, say "Sorry, I don't know." \
+        `},
+        {
+          role: 'user',
+          content: prompt,
+        }
+      ],
       temperature: 0.1,
       max_tokens: 512,
       stream: true
@@ -60,7 +59,7 @@ export const OpenAIStream = async (Prompt: string, apiKey: string) => {
 
           try {
             const json = JSON.parse(data);
-            const text = json.choices[0].text;
+            const text = json.choices[0].message.content;
             const queue = encoder.encode(text);
             controller.enqueue(queue);
           } catch (e) {
